2931162f2357803da7ebccecb98a15b696b356a0
GitMuhammadS <mdshahbazengg@gmail.com>
2024-08-30 09:19:17 +0530

    from team lead screen

---
+++ TeamLeadProject/src/main/java/com/shahbaz/TeamLeadProjectApplication.java
@@ -0,0 +1,13 @@
+package com.shahbaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamLeadProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamLeadProjectApplication.class, args);
+	}
+
+}
